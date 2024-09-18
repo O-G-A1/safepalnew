f32a8cf7b7408b56b05814857b6f025fe0e15982
Gbenga Ogunsola <gbengaadeboye99@gmail.com>
2024-09-18 07:43:05 +0100

    js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 function savedata(){
     const firstName=document.getElementById("Firstname").value;
-    localStorage.setItem("First Name", firstName);
+    localStorage.setItem("Firstname", firstName);
     const email=document.getElementById("Email").value;
     localStorage.setItem("Email", email);
 
@@ -9,6 +9,6 @@ function savedata(){
 function mention(){
     const firstName=localStorage.getItem("Firstname");
     const email=localStorage.getItem("Email");
-    document.querySelector(".usernameonenable").textContent=firstName;
-    document.getElementById("emailaddress").textContent=`" " + ${email}`;
+    document.getElementById("username").textContent=firstName;
+    document.getElementById("emailaddress").textContent=email;
 }
