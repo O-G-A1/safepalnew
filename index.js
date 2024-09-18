43f112738c5affe28846b4b8c2f6a12e8a94ce59
Gbenga Ogunsola <gbengaadeboye99@gmail.com>
2024-09-18 08:26:01 +0100

    js3

--- index.js
+++ index.js
@@ -12,3 +12,17 @@ function mention(){
     document.getElementById("username").textContent=firstName;
     document.getElementById("emailaddress").textContent=email;
 }
+
+
+function savedatab(){
+    const amount=document.getElementById("amount").value;
+    localStorage.setItem("Withdrawalamount", amount);
+    const tag=document.getElementById("cashapptag").value;
+    localStorage.setItem("Cashapptag", tag);
+}
+function state(){
+    const tag=localStorage.getItem("Cashapptag");
+    const amount=localStorage.getItem("Withdrawalamount");
+    document.getElementById("withdrawnamount").textContent=amount;
+    document.getElementById("tag").textContent=`tag: ${tag}`;
+}
